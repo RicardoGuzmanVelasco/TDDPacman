bdb676742cc8604d2028c9eda8f3dbc65ab32323
RGV <ricardo.guzman.velasco@outlook.com>
2024-09-24 11:16:25 +0200

    refactor: extract direction to tuple

--- Pacman.Domain.Tests/Board.cs
+++ Pacman.Domain.Tests/Board.cs
@@ -1,10 +1,12 @@
-﻿namespace Pacman.Domain.Tests;
+﻿using System.Diagnostics;
+
+namespace Pacman.Domain.Tests;
 
 public class Board
 {
     Direction pacmanDirection = Direction.None;
     (int x, int y) pacmanPosition = (0, 0);
-    
+
     public (int x, int y) WhereIsPacman()
     {
         return pacmanPosition;
@@ -17,17 +19,28 @@ public class Board
 
     public void Tick()
     {
+        MovePacman();
+    }
+
+    void MovePacman()
+    {
         if (pacmanDirection == Direction.None)
             return;
-        
-        if (pacmanDirection == Direction.Down)
-            pacmanPosition = (0, -1);
-        if (pacmanDirection == Direction.Up)
-            pacmanPosition = (0, 1);
+
+        var x = pacmanPosition.x + WhereToMovePacmanTowards().x;
+        var y = pacmanPosition.y + WhereToMovePacmanTowards().y;
+        pacmanPosition = (x, y);
+    }
+
+    (int x, int y) WhereToMovePacmanTowards()
+    {
+        Debug.Assert(WhereIsPacmanLookingTowards() != Direction.None);
+        return WhereIsPacmanLookingTowards().ToTuple();
     }
 
     public void PacmanLooksTowards(Direction towards)
     {
+        Debug.Assert(towards != Direction.None);
         pacmanDirection = towards;
     }
 }
--- Pacman.Domain.Tests/Direction.cs
+++ Pacman.Domain.Tests/Direction.cs
@@ -6,3 +6,17 @@ public enum Direction
     Up,
     Down
 }
+
+public static class DirectionToTuple
+{
+    public static (int x, int y) ToTuple(this Direction direction)
+    {
+        return direction switch
+        {
+            Direction.None => (0, 0),
+            Direction.Up => (0, 1),
+            Direction.Down => (0, -1),
+            _ => throw new InvalidOperationException()
+        };
+    }
+}
