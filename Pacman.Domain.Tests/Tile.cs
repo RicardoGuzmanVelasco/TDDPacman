449560db754773acf1791246d304a1a90c2c2d48
RGV <ricardo.guzman.velasco@outlook.com>
2024-10-11 12:00:07 +0200

    refactor: coord is tile now, next to a tile

---
+++ Pacman.Domain.Tests/Tile.cs
@@ -0,0 +1,20 @@
+﻿namespace Pacman.Domain.Tests;
+
+public readonly struct Tile
+{
+    public int X { get; }
+    public int Y { get; }
+
+    public Tile(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    
+    public static Tile Zero => new(0, 0);
+    
+    public Tile NextTo(Direction direction) => this + direction.ToTuple();
+    
+    public static Tile operator +(Tile a, Tile b) => new(a.X + b.X, a.Y + b.Y);
+    public static Tile operator +(Tile a, (int x, int y) b) => new(a.X + b.x, a.Y + b.y);
+}
